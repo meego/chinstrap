713b5ebd21ca8356914e322bb2fce30e20c23e65
Susan Tu <susanctu@gmail.com>
2015-05-01 14:31:01 -0700

    try again

--- emptyheaded/tests/test_all.sh
+++ emptyheaded/tests/test_all.sh
@@ -4,5 +4,15 @@
 cd ./emptyheaded
 source ./libs/build.sh
 source ./libs/tbbvars.sh intel64
+make libs
 make tests
+make libs
+export PATH=$PATH:/home/travis/build/craberger/chinstrap/emptyheaded/libs/zeromq-4.0.5/include
+export CPATH=$CPATH:/home/travis/build/craberger/chinstrap/emptyheaded/libs/zeromq-4.0.5/include
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/travis/build/craberger/chinstrap/emptyheaded/libs/zeromq-4.0.5/include
+export LIBRARY_PATH=LIBRARY_PATH:/home/travis/build/craberger/chinstrap/emptyheaded/libs/zeromq-4.0.5/include
+make server
+./bin/server &
+cd ../duncecap
+./binariesForTravis/sbt-for-travis test
 cd ..
